f87c30b3fa825e44059d7e96fb99a8e2edc2606e
小虎哥 <529901956@qq.com>
2019-11-30 14:17:42 +0800

    v1.2.0版本更新 [新增]Tags标签主页； [新增]小区模块 [新增]二手房模块 [新增]租房模块 [新增]会员中心 [新增]后台二手房、租房列表增加“显示”筛选项，用于审核会员发布是否显示在网站 [优化]arclist标签增加screen属性，screen='1'使用当前页面筛选条件，screen='0'为不使用，默认值为'1' [优化]区域设置启用-设置默认自动关联 [优化]楼盘、小区填写地址后，自动获取定位数据 [优化]新增楼盘时，填写户型面积，自动根据均价计算出户型总价 [优化]停车位、户数等可根据实际情况是否设置单位，且同步到后台编辑与前台显示 [安全]修补国家漏洞库反馈的一些已知漏洞； [优化]子站点域名不存在，直接跳转404错误； [优化]瀑布流JS语法，提高错误的兼容性； [优化]自定义日期字段的标签调用语法； [优化]留言发送邮箱超时从5秒改为10秒； [优化]arclist标签调用可以不走当前页面screning查询条件，screen='0' [优化]编辑区域自动填充地图坐标 [优化]添加房源数据，SEO标题自动同步房源标题 [修复]sqlview标签查询addwhere查询条件无效 [修复]报名表单管理新增、删除表单信息异常 [修复]修复开启子站点之后，手机站访问出现404错误 [修复]瀑布流加载数据不对的BUG； [修复]手机站访问支持域名类似.com.cn两个相连后缀； [修复]周边配套（百度地图）缩放问题

---
+++ data/schema/eju_users_content.php
@@ -0,0 +1,192 @@
+<?php 
+return array (
+  'id' => 
+  array (
+    'name' => 'id',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => NULL,
+    'primary' => true,
+    'autoinc' => true,
+  ),
+  'users_id' => 
+  array (
+    'name' => 'users_id',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'status' => 
+  array (
+    'name' => 'status',
+    'type' => 'tinyint(1)',
+    'notnull' => false,
+    'default' => '1',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'is_del' => 
+  array (
+    'name' => 'is_del',
+    'type' => 'tinyint(1) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'last_login' => 
+  array (
+    'name' => 'last_login',
+    'type' => 'int(11)',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'last_ip' => 
+  array (
+    'name' => 'last_ip',
+    'type' => 'varchar(15)',
+    'notnull' => false,
+    'default' => '',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'login_count' => 
+  array (
+    'name' => 'login_count',
+    'type' => 'int(11)',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'all_send' => 
+  array (
+    'name' => 'all_send',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'ershou_send' => 
+  array (
+    'name' => 'ershou_send',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'zufang_send' => 
+  array (
+    'name' => 'zufang_send',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'all_top' => 
+  array (
+    'name' => 'all_top',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'ershou_top' => 
+  array (
+    'name' => 'ershou_top',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'zufang_top' => 
+  array (
+    'name' => 'zufang_top',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'day_send' => 
+  array (
+    'name' => 'day_send',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'day_ershou_send' => 
+  array (
+    'name' => 'day_ershou_send',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'day_zufang_send' => 
+  array (
+    'name' => 'day_zufang_send',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'day_top' => 
+  array (
+    'name' => 'day_top',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'day_ershou_top' => 
+  array (
+    'name' => 'day_ershou_top',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'day_zufang_top' => 
+  array (
+    'name' => 'day_zufang_top',
+    'type' => 'int(11) unsigned',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'add_time' => 
+  array (
+    'name' => 'add_time',
+    'type' => 'int(11)',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+  'update_time' => 
+  array (
+    'name' => 'update_time',
+    'type' => 'int(11)',
+    'notnull' => false,
+    'default' => '0',
+    'primary' => false,
+    'autoinc' => false,
+  ),
+);
