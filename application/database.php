6f7fdc100aa27a7b43afd6a2c169d83cb541e641
小虎哥 <529901956@qq.com>
2020-02-05 08:47:23 +0800

    no commit message

--- application/database.php
+++ application/database.php
@@ -15,13 +15,13 @@ return array(
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '121.41.77.4',
     // 数据库名
-    'database'        => 'meizeju',
+    'database'        => 'ejucms_demo4',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => 'chengx2008',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
